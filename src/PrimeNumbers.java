a4a6a7301456314094c8cf246cf492392f060c74
haliax11 <paco-j-m11@hotmail.com>
2017-10-06 19:39:22 +0200

    proyecto final

---
+++ src/PrimeNumbers.java
@@ -0,0 +1,24 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int i=1;
+        while (i <= 10000) {
+            boolean prime = true;
+            int div = i - 1;
+            while (div >= 2) {
+                if (i % div == 0) {
+                    prime = false;
+
+                }
+                div = div - 1;
+            }
+            if (prime) {
+                System.out.println(i);
+            }
+            i++;
+            if (i % 1000 == 0) {
+                System.out.println("----------------------------------------------");
+            }
+        }
+    }
+
+}
