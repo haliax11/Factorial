e6e818a28ee5044218062c9575e646f18466770a
haliax11 <paco-j-m11@hotmail.com>
2017-10-06 20:23:58 +0200

    version 1.1

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,6 +1,6 @@
 public class PrimeNumbers {
     public static void main(String[] args) {
-        int i=1;
+        int i=2;
         while (i <= 10000) {
             boolean prime = true;
             int div = i - 1;
